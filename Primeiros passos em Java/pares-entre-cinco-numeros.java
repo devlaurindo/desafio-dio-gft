a7bb1ed1fc7740e7267fe945399d4cf9763b62ef
devlaurindo <user-t00r@hotmail.com>
2021-09-14 09:18:31 -0300

    resolução de desafio em java primeiros passos

---
+++ Primeiros passos em Java/pares-entre-cinco-numeros.java
@@ -0,0 +1,38 @@
+/*Faça um programa que leia 5 valores inteiros. Conte quantos destes valores digitados são pares e mostre esta informação.
+
+Entrada
+O arquivo de entrada contém 5 valores inteiros quaisquer.
+
+Saída
+Imprima a mensagem conforme o exemplo fornecido, indicando a quantidade de valores pares lidos.
+
+ 
+Exemplo de Entrada	Exemplo de Saída
+7                   3 valores pares
+-5
+6
+-4
+12 
+
+*/
+
+import java.util.Scanner;
+
+public class Problem{
+
+	public static void main(String[] args) throws Exception {
+		Scanner scan = new Scanner(System.in);
+		int count = 0;
+		
+		for(int i = 0; i < 5; i++) {
+			int n = scan.nextInt();
+
+      if ((n % 2) == 0) count++;
+            
+			//complete com sua solução aqui
+		}
+
+		
+		System.out.printf("%d valores pares\n", count);
+	}
+}
